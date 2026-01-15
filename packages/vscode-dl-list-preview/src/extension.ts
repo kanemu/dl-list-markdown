70980118ffe255ac6019b8199bb487d8f30a73f8
Yohei Kanamura <nc.kanamura@gmail.com>
2026-01-15 22:31:30 +0900

    Hide the left line that appears when hovering over <dl> elements

--- packages/vscode-dl-list-preview/src/extension.ts
+++ packages/vscode-dl-list-preview/src/extension.ts
@@ -4,9 +4,21 @@ import dlList from "markdown-it-dl-list";
 export function activate() {
     return {
         extendMarkdownIt(md: any) {
+            md.core.ruler.push('remove_dl_source_map', (state: any) => {
+                state.tokens.forEach((token: any) => {
+                    if (token.type === 'dl_list_open') {
+                        token.map = null;
+                        if (token.attrs) {
+                            token.attrs = token.attrs.filter((attr: [string, string]) =>
+                                attr[0] !== 'data-line' && attr[0] !== 'class'
+                            );
+                        }
+                    }
+                });
+            });
+
             const cfg = vscode.workspace.getConfiguration("dlListPreview");
             const ddIndent = clampInt(cfg.get<number>("ddIndent", 4), 1, 12);
-
             md.use(dlList, { ddIndent });
             return md;
         }
